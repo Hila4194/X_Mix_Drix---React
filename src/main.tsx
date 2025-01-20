acad41fc8064f914fb2588dad1fc5e31ea2c0e35
Elrom5 <elrombenami12@gmail.com>
2025-01-20 10:01:09 +0200

    building the logic

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-// Use TypeScript to ensure the root element is not null
+/* Use TypeScript to ensure the root element is not null*/
+
 const rootElement = document.getElementById('root') as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
